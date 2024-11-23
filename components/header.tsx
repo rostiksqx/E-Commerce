81d8dd2fb89e6b8d35e746b3521b38501642d395
Rostyslav <rostikboss2005@gmail.com>
2024-11-23 19:35:48 +0200

    responsive design for header, product type defined

--- components/header.tsx
+++ components/header.tsx
@@ -11,6 +11,7 @@ import {
   NavigationMenuTrigger,
 } from "@/components/ui/navigation-menu";
 import ListItem from "@/components/ui/list-item";
+import * as React from "react";
 
 const linkList: {
   title: string;
@@ -45,15 +46,18 @@ export default function Header() {
   const { user } = useUser();
 
   return (
-    <header className="flex items-center w-full px-[100px] py-6">
+    <header className="flex items-center justify-center w-full px-[100px] py-6 max-lg:py-5 max-lg:px-4">
+      <div className="hidden max-lg:block mr-4 mt-1">
+        <Image src="/menu.svg" alt="menu" width="24" height="24" />
+      </div>
+
       <Link href="/">
-        <span className="font-integralCFRegular font-bold text-3xl">
+        <span className="font-integralCFRegular font-bold text-[32px] max-lg:text-2xl">
           SHOP.CO
         </span>
       </Link>
 
-      {/*<nav className="font-satoshiRegular flex items-center space-x-6 mx-10">*/}
-      <NavigationMenu className="font-satoshiRegular flex items-center space-x-6 mx-10">
+      <NavigationMenu className="font-satoshiRegular flex items-center space-x-6 mx-10 max-lg:hidden">
         <NavigationMenuList>
           <NavigationMenuItem>
             <NavigationMenuTrigger className="p-2 font-satoshiRegular text-base">
@@ -86,19 +90,20 @@ export default function Header() {
         </Link>
       </NavigationMenu>
 
-      <div className="flex items-center flex-1 space-x-3.5">
-        <div className="relative flex-1 mr-10">
+      <div className="flex items-center flex-1 space-x-3.5 max-lg:justify-end max-lg:space-x-3">
+        <div className="relative flex-1 mr-10 max-lg:flex-initial max-lg:m-0">
           <Image
             src="/search.svg"
             alt="Search"
             width="24"
             height="24"
-            className="absolute left-3 top-1/2 transform -translate-y-1/2"
+            className="absolute left-3 top-1/2 transform -translate-y-1/2 max-lg:static
+            max-lg:left-0 max-lg:top-0 max-lg:translate-y-0 "
           />
           <input
             type="text"
             placeholder="Search for products..."
-            className="bg-[#F0F0F0] text-[#000000] border-none focus:outline-none pl-10 pr-4 py-3 w-full rounded-3xl"
+            className="bg-[#F0F0F0] text-[#000000] border-none focus:outline-none pl-10 pr-4 py-3 w-full rounded-3xl max-lg:hidden"
           />
         </div>
         <ClerkLoaded>
---
+++ sanity/schemaTypes/productType.ts
@@ -0,0 +1,133 @@
+﻿import { defineArrayMember, defineField, defineType } from "sanity";
+import { ShirtIcon } from "lucide-react";
+
+export const productType = defineType({
+  name: "product",
+  title: "Product",
+  type: "document",
+  icon: ShirtIcon,
+  fields: [
+    defineField({
+      name: "name",
+      title: "Product Title",
+      type: "string",
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      options: {
+        source: "name",
+        maxLength: 96,
+      },
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: "reviews",
+      title: "Reviews",
+      type: "array",
+      of: [
+        defineArrayMember({
+          type: "object",
+          fields: [
+            {
+              name: "rate",
+              title: "Rate",
+              type: "number",
+              validation: (Rule) => Rule.required().min(0).max(5),
+            },
+            {
+              name: "comment",
+              title: "Comment",
+              type: "string",
+            },
+            {
+              name: "user",
+              title: "User",
+              type: "reference",
+              to: { type: "user" },
+              validation: (Rule) => Rule.required(),
+            },
+          ],
+        }),
+      ],
+    }),
+    defineField({
+      name: "rating",
+      title: "Rating",
+      type: "number",
+      readOnly: true,
+      validation: (Rule) => Rule.min(0).max(5),
+    }),
+    defineField({
+      name: "images",
+      title: "Images",
+      type: "array",
+      of: [defineArrayMember({ type: "image" })],
+    }),
+    defineField({
+      name: "smallDescription",
+      title: "smallDescription",
+      type: "string",
+    }),
+    defineField({
+      name: "productDetails",
+      title: "Product Details",
+      type: "blockContent",
+    }),
+    defineField({
+      name: "price",
+      title: "Price",
+      type: "number",
+      validation: (Rule) => Rule.required().min(0),
+    }),
+    defineField({
+      name: "discount",
+      title: "Discount",
+      type: "number",
+      validation: (Rule) => Rule.required().min(0).max(100),
+    }),
+    defineField({
+      name: "colors",
+      title: "Colors",
+      type: "array",
+      of: [
+        defineArrayMember({
+          type: "string",
+          validation: (Rule) =>
+            Rule.required().custom((color) => {
+              if (typeof color === "string" && color.startsWith("#")) {
+                return true;
+              }
+              return "Color must begin with #";
+            }),
+        }),
+      ],
+    }),
+    defineField({
+      name: "sizes",
+      title: "Sizes",
+      type: "array",
+      of: [
+        defineArrayMember({
+          type: "string",
+          options: {
+            list: [
+              { title: "Small", value: "S" },
+              { title: "Medium", value: "M" },
+              { title: "Large", value: "L" },
+              { title: "X-Large", value: "XL" },
+            ],
+          },
+        }),
+      ],
+    }),
+    defineField({
+      name: "categories",
+      title: "Categories",
+      type: "array",
+      of: [defineArrayMember({ type: "reference", to: { type: "category" } })],
+    }),
+  ],
+});
